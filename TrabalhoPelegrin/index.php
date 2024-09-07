97d096398488e9d99df45774247af9877f5cb19e
Gabriel Pio Machado <gabrielpiomachado680@gmail.com>
2024-09-07 16:02:56 -0300

    Estrutura do Formulário HTML

--- TrabalhoPelegrin/index.php
+++ TrabalhoPelegrin/index.php
@@ -8,6 +8,22 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Formulário</h1>
+    <div class="container mt-5">
+        <form action="./src/php/processamento.php" method="POST">
+            <div class="form-group">
+                <label for="nome">Nome:</label>
+                <input required type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome">
+            </div>
+            <div class="form-group">
+                <label for="dataNascimento">Data de Nascimento:</label>
+                <input required type="date" class="form-control" id="dataNascimento" name="dataNascimento">
+            </div>
+            <div class="form-group">
+                <label for="descricao">Descrição:</label>
+                <textarea class="form-control" id="descricao" name="descricao" rows="5" placeholder="Escreva uma descrição"></textarea>
+            </div>
+            <button type="submit" class="btn btn-success">Enviar Formulário</button>
+        </form>
+    </div>
 </body>
 </html>
