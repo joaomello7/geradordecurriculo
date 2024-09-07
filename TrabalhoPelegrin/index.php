e1a4b428a63542a6e48b70f9e71e5f7d9f283654
Gabriel Pio Machado <gabrielpiomachado680@gmail.com>
2024-09-07 15:58:48 -0300

    Cabeçalho com Favicon e Bootstrap

--- TrabalhoPelegrin/index.php
+++ TrabalhoPelegrin/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerador de Currículo</title>
+    <link rel="shortcut icon" type="imagex/png" href="./src/images/macaco.png">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <h1>Formulário</h1>
